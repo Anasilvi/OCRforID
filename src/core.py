ea25f68ccc441430eba98b781b6802c09be14b04
Anasilvia Salazar <anasilvia82@gmail.com>
2021-08-05 18:41:36 -0500

    Deleted functions not used in core.py

--- src/core.py
+++ src/core.py
@@ -17,53 +17,6 @@ minCI = 90
 
 processFinish = False
 
-# get grayscale image
-def get_grayscale(image):
-    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# noise removal
-def remove_noise(image):
-    return cv2.medianBlur(image,3)
- 
-#thresholding
-def thresholding(image):
-    #return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    return cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,10)
-
-#dilation
-def dilate(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
-    
-#erosion
-def erode(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.erode(image, kernel, iterations = 1)
-
-#opening - erosion followed by dilation
-def opening(image):
-    kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-
-#canny edge detection
-def canny(image):
-    return cv2.Canny(image, 100, 200)
-
-#skew correction
-def deskew(image):
-    coords = np.column_stack(np.where(image > 0))
-    angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
-        angle = -angle
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return rotated
-
 #template matching
 def match_template(image, template):
     h, w = template.shape[:2]
@@ -115,9 +68,9 @@ def cutImage(image):
 def processImage(path):
     imgOriginal = cv2.imread(path)
     imgProcessed, imgReSized, deltaX, deltaY = match_template(reScaling(cutImage(imgOriginal)),imgTemplate)[:4]
-    img = get_grayscale(imgProcessed)
-    img = remove_noise(img)
-    img = thresholding(img)
+    img = cv2.cvtColor(imgProcessed, cv2.COLOR_BGR2GRAY)
+    img = cv2.medianBlur(img,3)
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
     processFinish, newImg = getData(img, imgReSized, deltaX, deltaY)[:2]
 
     if processFinish:
